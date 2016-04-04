f0649acd5a3197881546cc35ef9b212fe84d12bd
zersh <zersh@mail.ru>
2016-04-04 14:44:08 +0300

    first commit

---
+++ ChocoMon/ChocoMon/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ChocoMon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Check = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkTray = new System.Windows.Forms.CheckBox();
+            this.Edit = new System.Windows.Forms.Button();
+            this.checkStartUp = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(85, 10);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(58, 24);
+            this.Check.TabIndex = 0;
+            this.Check.Text = "Check";
+            this.toolTip1.SetToolTip(this.Check, "Check now");
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.textBox1, "Seconds to update");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.toolTip1.SetToolTip(this.button1, "Start/Stop autoupdate. Default allways enables an startup");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkTray
+            // 
+            this.checkTray.AutoSize = true;
+            this.checkTray.Location = new System.Drawing.Point(85, 141);
+            this.checkTray.Name = "checkTray";
+            this.checkTray.Size = new System.Drawing.Size(47, 17);
+            this.checkTray.TabIndex = 9;
+            this.checkTray.Text = "Tray";
+            this.toolTip1.SetToolTip(this.checkTray, "Miniminize to tray");
+            this.checkTray.UseVisualStyleBackColor = true;
+            this.checkTray.CheckedChanged += new System.EventHandler(this.checkTray_CheckedChanged);
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(85, 40);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(58, 24);
+            this.Edit.TabIndex = 10;
+            this.Edit.Text = "Edit";
+            this.toolTip1.SetToolTip(this.Edit, "Edit URLs");
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Edit_Click);
+            // 
+            // checkStartUp
+            // 
+            this.checkStartUp.AutoSize = true;
+            this.checkStartUp.Location = new System.Drawing.Point(85, 164);
+            this.checkStartUp.Name = "checkStartUp";
+            this.checkStartUp.Size = new System.Drawing.Size(68, 17);
+            this.checkStartUp.TabIndex = 13;
+            this.checkStartUp.Text = "Autostart";
+            this.toolTip1.SetToolTip(this.checkStartUp, "Startup whith Windows");
+            this.checkStartUp.UseVisualStyleBackColor = true;
+            this.checkStartUp.CheckedChanged += new System.EventHandler(this.checkStartUp_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(91, 211);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Update";
+            this.toolTip1.SetToolTip(this.checkBox1, "Check new version");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "URL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Downloads";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Timer";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ChocoMon";
+            this.notifyIcon1.Visible = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkStartUp);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.Edit);
+            this.groupBox1.Controls.Add(this.checkTray);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.Check);
+            this.groupBox1.Location = new System.Drawing.Point(503, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 276);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Time Load:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Version";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 247);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(58, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "1.0.0.1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 282);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChocoMon";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox checkTray;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkStartUp;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ChocoMon/ChocoMon/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+using System.Text.RegularExpressions;
+using System.Net;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace ChocoMon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            notifyIcon1.Visible = false;
+            this.notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseDoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+
+            //Read period and * 1000 to seconds
+            string UpdatePeriod = ConfigurationManager.AppSettings["UpdatePeriod"];
+            textBox1.Text = UpdatePeriod;
+            int UpdatePeriodNum = Convert.ToInt32(UpdatePeriod);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = UpdatePeriodNum * 1000;
+
+            // Enable timer.
+            timer1.Enabled = true;
+            button1.Text = "Stop";
+
+            //Checked checkbox Tray an startup
+            string MinimizeToTray = ConfigurationManager.AppSettings["MinimizeToTray"];
+            if (MinimizeToTray == "1")
+            {
+                checkTray.Checked = true;
+            }
+            else
+            {
+                checkTray.Checked = false;
+            }
+            //checked autostart
+            //Checked checkbox Tray an startup
+            string Autostart = ConfigurationManager.AppSettings["Autostart"];
+            if (Autostart == "1")
+            {
+                checkStartUp.Checked = true;
+            }
+            else
+            {
+                checkStartUp.Checked = false;
+            }
+            //check update
+            string Update = ConfigurationManager.AppSettings["Update"];
+            if (Update == "1")
+            {
+                checkBox1.Checked = true;
+                CheckUpdate();
+            }
+
+        }
+        string WorkDir = System.Windows.Forms.Application.StartupPath;
+        //For grouping dynamic elements
+        GroupBox new_groupBox = new GroupBox();
+        int A = 3;
+        int B = 3;
+        int C = 3;
+
+        private void Check_Click(object sender, EventArgs e)
+
+        {
+            //время выполнения
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            //save timer to config file
+            SaveTimer();
+
+            //очищаем форму
+            new_groupBox.Controls.Clear();
+
+            new_groupBox.Height = 500;
+            new_groupBox.Width = 500;
+            this.Controls.Add(new_groupBox);
+
+
+            string URL;
+            // Read the file and display it line by line.
+            //string WorkDir = System.Windows.Forms.Application.StartupPath;
+            System.IO.StreamReader file = new System.IO.StreamReader(WorkDir + @"\\UrlList.txt");
+            while ((URL = file.ReadLine()) != null)
+            {
+                AddNewTextBox(URL);
+                AddNewTextBox2(URL);
+
+            }
+
+            file.Close();
+            A = 3;
+            B = 3;
+            C = 3;
+
+            //time stop and add to textbox 
+            sw.Stop();
+            //в секундах
+            textBox2.Text = (sw.ElapsedMilliseconds / 1000.0).ToString();
+
+        }
+
+        public System.Windows.Forms.TextBox AddNewTextBox(string URL)
+        {
+            TextBox txt = new TextBox();
+            this.Controls.Add(txt);
+            txt.Top = A * 10;
+            txt.Left = 45;
+            //set string from app.config
+            txt.Text = URL;
+            txt.Width = 300;
+            txt.ReadOnly = true;
+            A = A + 3;
+            new_groupBox.Controls.Add(txt);
+            return txt;
+        }
+
+        //Download stats
+        public System.Windows.Forms.TextBox AddNewTextBox2(string URL)
+        {
+
+            //string URL = ConfigurationManager.AppSettings[ii];
+
+            // Создаём экземпляр класса
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            // Присваиваем текстовой переменной k html-код
+            WebClient webClient = new WebClient();
+            string PageIn = webClient.DownloadString(URL);
+            // Загружаем в класс (парсер) наш html
+            doc.LoadHtml(PageIn);
+            //получить нужны код для ноды можно через среду разработки хрома - копируется необходимы кусок как 
+            //Copy as XPath
+            //HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//div[@class='stat']/stat-number");
+
+            HtmlNode FullStatNum = doc.DocumentNode.SelectSingleNode("//*[@id='stats']/div[1]/p[1]");
+            HtmlNode PackageStatNum = doc.DocumentNode.SelectSingleNode("//*[@id='stats']/div[2]/p[1]");
+            // Выводим на экран результат работы парсера
+            //MessageBox.Show(bodyNode.InnerText);
+            //MessageBox.Show(bodyNode.Attributes["stat-number"].Value);
+
+            //Get image status and sent to AddNewPictureBox
+            HtmlNode ImageStatus = doc.DocumentNode.SelectSingleNode("//*[@id='mainColumn']/div[1]/div[1]/h2[2]/a/img");
+            AddNewPictureBox(ImageStatus.Attributes["src"].Value, ImageStatus.Attributes["alt"].Value, URL);
+
+            TextBox txt2 = new TextBox();
+            this.Controls.Add(txt2);
+            txt2.Top = B * 10;
+            txt2.Left = 370;
+            txt2.Width = 63;
+            txt2.ReadOnly = true;
+            txt2.Text = PackageStatNum.InnerText;
+            new ToolTip().SetToolTip(txt2, FullStatNum.InnerText + " All downloads");
+            B = B + 3;
+
+            new_groupBox.Controls.Add(txt2);
+            return txt2;
+        }
+
+        //image Status
+        public System.Windows.Forms.PictureBox AddNewPictureBox(string ImageStatus, string StatusComment, string URL)
+        {
+            /*
+            string URL = ConfigurationManager.AppSettings[i];
+            WebClient webClient = new WebClient();
+
+            string PageIn = webClient.DownloadString(URL);
+            var ball = Regex.Match(PageIn, @"\/content.*_ball_...png", RegexOptions.IgnoreCase).Value;
+
+            PictureBox pict = new PictureBox();
+            //string StatusImageURL = URL + ball;
+            //pict.Load(StatusImageURL);
+            //MessageBox.Show(ImageStatus);
+            */
+
+            PictureBox pict = new PictureBox();
+            if (ImageStatus == "/content/images/green_ball_48.png")
+            {
+                pict.Load(WorkDir + "\\Images/green_ball_48.png");
+            }
+            else if (ImageStatus == "/content/images/grey_ball_48.png")
+            {
+                pict.Load(WorkDir + "\\Images/grey_ball_48.png");
+            }
+            else if (ImageStatus == "/content/images/yellow_ball_48.png")
+            {
+                pict.Load(WorkDir + "\\Images/yellow_ball_48.png");
+            }
+            else if (ImageStatus == "/content/images/red_ball_48.png")
+            {
+                pict.Load(WorkDir + "\\Images/red_ball_48.png");
+            }
+            else
+            {
+                pict.Load("https://chocolatey.org" + ImageStatus);
+            }
+
+            this.Controls.Add(pict);
+            pict.Top = C * 10;
+            pict.Left = 10;
+            pict.Width = 20;
+            pict.Height = 20;
+            pict.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            new ToolTip().SetToolTip(pict, StatusComment + "Please double click to visit web site for more information.");
+            pict.Click += (sender, e) => { System.Diagnostics.Process.Start(URL); };
+
+            //pict.Text = "pictBox" + this.C.ToString();
+            C = C + 3;
+            new_groupBox.Controls.Add(pict);
+            return pict;
+        }
+
+        //run check when timer tic
+        private void timer1_Tick(object Sender, EventArgs e)
+        {
+            Check_Click(Check, null);
+        }
+        private void SaveTimer()
+        {
+            //save timer to config file
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            config.AppSettings.Settings["UpdatePeriod"].Value = textBox1.Text;
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("UpdatePeriod");
+            Properties.Settings.Default.Reload();
+
+            int UpdatePeriodNum = Convert.ToInt32(textBox1.Text);
+            timer1.Interval = UpdatePeriodNum * 1000;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                SaveTimer();
+                button1.Text = "Start";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                SaveTimer();
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+        }
+        //minimize to tray
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // проверяем наше окно, и если оно было свернуто, делаем событие        
+            if (WindowState == FormWindowState.Minimized && checkTray.Checked)
+            {
+                // прячем наше окно из панели
+                this.ShowInTaskbar = false;
+                // делаем нашу иконку в трее активной
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon1.BalloonTipTitle = "ChocoMon";
+                notifyIcon1.BalloonTipText = "Now ChocoMon in tray";
+                notifyIcon1.ShowBalloonTip(5);
+
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // делаем нашу иконку скрытой
+            notifyIcon1.Visible = false;
+            // возвращаем отображение окна в панели
+            this.ShowInTaskbar = true;
+            //разворачиваем окно
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void checkTray_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkTray.Checked)
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings["MinimizeToTray"].Value = "1";
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            else
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings["MinimizeToTray"].Value = "0";
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+
+        //Open UrlList.txt 
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            //string WorkDir = System.Windows.Forms.Application.StartupPath;
+            //MessageBox.Show(WorkDir);
+            System.Diagnostics.Process.Start (WorkDir+"\\UrlList.txt");
+        }
+
+        
+        //Set startup when windows start
+        private void checkStartUp_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkStartUp.Checked)
+            {
+                rk.SetValue("ChocoMon", Application.ExecutablePath.ToString());
+            }
+            else {
+                rk.DeleteValue("ChocoMon", false);
+            }
+        }
+        //Check Updates
+        private void CheckUpdate()
+        {
+            //First check internet connection
+            try
+            {
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead("http://www.google.com"))
+                {
+                    //If available -> Check update
+                    string TempDir = Path.GetTempPath();
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile("https://raw.githubusercontent.com/zersh01/ChocoMain/master/InstallChocoMaint/latest.txt", TempDir + "\\latest.txt");
+
+                    string LatestVersion = TempDir + "\\latest.txt";
+
+                    using (StreamReader sr = new StreamReader(LatestVersion))
+                    {
+                        string CurVersion = textBox3.Text;
+                        string NewVersion = sr.ReadToEnd();
+
+                        if (CurVersion != NewVersion)
+                        {
+
+                            textBox3.ForeColor = Color.DarkRed;
+                            textBox3.BackColor = Color.Yellow;
+                            textBox3.Text = NewVersion;
+
+                            ToolTip updtooltip = new ToolTip();
+                            updtooltip.SetToolTip(textBox3, "Avalible new version. Press double click to go websuite.");
+
+                            //Close and Delete file
+                            sr.Close();
+                            System.IO.File.Delete(LatestVersion);
+                        }
+                        else
+                        {
+                            //Close and Delete file latest
+                            sr.Close();
+                            System.IO.File.Delete(LatestVersion);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Don't check updates. No Internet Connection. You may disable auto check in Help menu.");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings["Update"].Value = "1";
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            else
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings["Update"].Value = "0";
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+    }
+}
